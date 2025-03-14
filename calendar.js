bd08cf6c010e64c5a7d0f0faf29b45e18f297b30
Sungmin <gan.sungmin@gmail.com>
2025-03-14 13:43:07 -0700

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -10,7 +10,7 @@ let calendarMonth = currentMonth;
 let calendarYear = currentYear;
 let calendarDates = [];
 
-// --- --- --- //
+// For Making the Basic Calendar //
 
 const getCalendarDates = () => {
     //Get Previous Month's
@@ -113,4 +113,126 @@ e("previousMonth").addEventListener("click", () => {
     }
     resetCalendarClasses()
     fillCalendar()
+})
+
+// For Filling the Calendar with Bookings //
+
+let bookingsToDisplay = [];
+
+const extractBookings = () => {
+	calendarDates.forEach((date, i) => {
+  	let year = date.date.getFullYear();
+    let month = (date.date.getMonth() + 1 < 10) ? `0${date.date.getMonth() + 1}` : date.date.getMonth() + 1;
+    let day = (date.date.getDate() < 10) ? `0${date.date.getDate()}` : date.date.getDate();
+  	let matchingBookings = charterBookings.filter(booking => booking.Date == `${year}-${month}-${day}`)
+    matchingBookings.forEach((booking) => { 
+    	bookingsToDisplay.push({booking: booking, position: i+1}) 
+    })
+  })
+}
+
+const displayBookings = () => {
+	bookingsToDisplay.forEach((booking, i) => {
+  	// Render elements
+  	let bookingDiv = document.createElement("div");
+    bookingDiv.classList.add("div-block-11");
+    let bookingBadge = document.createElement("div");
+    bookingBadge.classList.add("text-block-6");
+    bookingBadge.setAttribute("id", `booking_${i}`)
+    bookingBadge.innerHTML = `${booking.booking.Time} | ${booking.booking["First Name"]} ${booking.booking["Last Name"]}`;
+    bookingDiv.appendChild(bookingBadge);
+    e(`datebox_${booking.position}`).appendChild(bookingDiv)
+    // Set event listener
+    bookingBadge.addEventListener("click", () => {
+    	populateBookingDetails(booking.booking)
+    	openBookingDetails()
+    })
+  })
+}
+
+// For Controlling Booking Details Form
+
+let dbFieldsDisabled = true;
+
+let bdElements = {
+    estimate: e("estimate"),
+  invoiced: e("invoiced"),
+  vessel: e("vessel"),
+  requestedDate: e("requestedDate"),
+  alternateDate: e("alternateDate"),
+	passengers: e("passengers"),
+  occasion: e("occasion"),
+  alcohol: e("alcohol"),
+  notes: e("notes"),
+  firstName: e("firstName"),
+  lastName: e("lastName"),
+  phone: e("phone"),
+  email: e("email"),
+  contactMode: e("contactMode"),
+  textOptIn: e("textOptIn")
+}
+
+const populateBookingDetails = (booking) => {
+	bdElements.passengers.value = booking.Passengers;
+  //bdElements.occasion.value = booking.Occasion;
+  //bdElements.alcohol.value = (booking.alcohol) ? "Yes" : "No";
+  bdElements.notes.value = booking["Additional Info"];
+  bdElements.firstName.value = booking["First Name"];
+  bdElements.lastName.value = booking["Last Name"];
+  bdElements.phone.value = booking.Phone;
+  bdElements.email.value = booking.Email;
+  bdElements.contactMode.value = booking["Contact Mode"];
+  bdElements.textOptIn.value = (booking["Text Opt In"]) ? "Yes" : "No";
+}
+
+const clearBookingDetails = (booking) => {
+	Object.keys(bdElements).forEach((key) => {
+  	bdElements[key].value = ""
+  })
+}
+
+const disableBDFields = () => {
+	Object.keys(bdElements).forEach((key) => {
+  	bdElements[key].disabled = true
+  })
+  if (!dbFieldsDisabled) {
+  	e("saveButton").classList.remove("show")
+    e("editButton").classList.remove("hidden")
+  }
+  dbFieldsDisabled = true;
+}
+
+const enableBDFields = () => {
+	Object.keys(bdElements).forEach((key) => {
+  	bdElements[key].disabled = false
+  })
+  e("editButton").classList.add("hidden")
+  e("saveButton").classList.add("show")
+  dbFieldsDisabled = false;
+}
+
+const openBookingDetails = () => {
+	disableBDFields();
+	e("bookingDetails").classList.add("open");
+}
+
+const closeBookingDetails = () => {
+	clearBookingDetails()
+	e("bookingDetails").classList.remove("open")
+}
+
+e("xBookingDetails").addEventListener("click", () => {
+	closeBookingDetails()
+})
+
+e("backscreenBookingDetails").addEventListener("click", () => {
+	closeBookingDetails()
+})
+
+e("editButton").addEventListener("click", () => {
+	enableBDFields()
+})
+
+e("saveButton").addEventListener("click", () => {
+	disableBDFields()
 })
