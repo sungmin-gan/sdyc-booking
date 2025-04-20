d24156fea35fdda2e0b0269caf673a3f894e3014
Sungmin <gan.sungmin@gmail.com>
2025-04-19 23:25:03 -0700

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -562,7 +562,7 @@ e("confirmSaveBooking_save").addEventListener("click", () => {
 })
 
 e("confirmSaveBooking_cancel").addEventListener("click", () => {
-    booking_update = { id: null, update: {} }
+    booking_update.update = {};
     e("confirmSaveBooking").classList.add("hidden");
 })
 
