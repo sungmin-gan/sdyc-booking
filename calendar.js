d63e475368fd2abb2faed0fccf7dc77f94b1a3a3
Sungmin <gan.sungmin@gmail.com>
2025-05-09 03:00:34 -0700

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -1,7 +1,5 @@
 //// //// //// //// Declarations //// //// //// ////
 
-const e = require("express");
-
 const WEEK = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const WEEKABBR = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
 const YEAR = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -558,7 +556,8 @@ function setBookingUpdate() {
             additional["vesselDisplayName"] = vessel.displayName;
         }
         else if (key == "status") {
-            if (!Object.keys(statusClass).contains(value)) { value = "" }
+            if (value == "newRequest") { value = "Vessel Request" }
+            else if (value == "findYacht") { value = "Send Options" }
         }
 
         let booking_value = booking[key];
